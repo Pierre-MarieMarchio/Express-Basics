68d9bb8556fb79fc1a6c61b2101321c6e1f10ae1
Pierre-Marie Marchio <pierremariemarchio@gmail.com>
2023-05-15 15:47:56 +0200

    routes

--- js/server.js
+++ js/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+const port = 3000
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.html')); 
+});
 
-app.listen(3000, function (){
-    console.log("server ssarted on port 3000");
+app.get("/contact", (req, res) => {
+    res.send("contact page is not yet implemented");
+});
+
+app.get("/about", (req, res) => {
+    res.send("about page not yet implemented");
+});
+
+app.listen(3000, () =>{
+    console.log("server ssarted on port " + port);
 });
